49e60ccaf5eb778c504a92709333e8cde4f9523b
PabloSzx <pablosaez1995@gmail.com>
2021-05-13 23:49:14 -0400

    change default anchor

--- packages/client/src/components.tsx
+++ packages/client/src/components.tsx
@@ -173,6 +173,8 @@ const td = (props: BoxProps) => (
   <Box as="td" p={2} borderTopWidth="1px" borderColor="inherit" fontSize="sm" whiteSpace="normal" {...props} />
 );
 
+const a = (props: LinkProps) => <ChakraLink fontWeight="bold" color="blue.600" {...props} />;
+
 export const components = {
   Image,
   h1,
@@ -188,7 +190,7 @@ export const components = {
   th,
   td,
   kbd: Kbd,
-  a: ChakraLink,
+  a,
   Link,
   ButtonLink,
   Button,
