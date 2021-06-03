a99f77b885bbc1f5698512324a21c478d137701c
PabloSzx <pablosaez1995@gmail.com>
2021-06-03 19:44:10 -0400

    exclude bin folder from hash

--- bob-esbuild.config.ts
+++ bob-esbuild.config.ts
@@ -1,6 +1,11 @@
 export const config: import('bob-esbuild').BobConfig = {
   tsc: {
     dirs: ['packages/*', '!packages/cli'],
+    hash: {
+      folders: {
+        exclude: ['bin'],
+      },
+    },
   },
   verbose: true,
   clean: false,
