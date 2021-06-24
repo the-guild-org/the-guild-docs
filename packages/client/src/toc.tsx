9d8ef051e6333c080cfa0bf915b9c56f765102f0
Alin Simoc <alin.s@softisfy.com>
2021-06-24 22:19:50 +0300

    ToC links overflow fix (#85)
    * toc links overflow fix
    
    * toc links overflow fix
    
    * changed to break word

--- packages/client/src/toc.tsx
+++ packages/client/src/toc.tsx
@@ -30,6 +30,7 @@ const Link = chakra('a', {
   baseStyle: {
     display: 'block',
     width: 'fit-content',
+    wordBreak: 'break-word',
     my: '0.5rem',
     fontSize: '0.875rem',
     transition: '0.15s',
