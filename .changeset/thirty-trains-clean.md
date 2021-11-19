8f0e267771793f9e1495407e7fede3404a7fb7c6
PabloSzx <pablosaez1995@gmail.com>
2021-11-19 15:47:08 -0300

    update

---
+++ .changeset/thirty-trains-clean.md
@@ -0,0 +1,5 @@
+---
+'@guild-docs/server': patch
+---
+
+Optional admonitions
--- packages/server/src/mdx.tsx
+++ packages/server/src/mdx.tsx
@@ -155,7 +155,7 @@ export interface CompiledMDX {
 
 const MdxDeps = LazyPromise(async () => {
   const [remarkAdmonitions, remarkEmoji, highlighter, withShiki, { serialize }, rehypeSlug] = await Promise.all([
-    import('remark-admonitions').then(v => v.default),
+    import('remark-admonitions').then(v => v.default).catch(() => void 0),
     import('remark-emoji').then(v => v.default),
     shiki.getHighlighter({
       theme: 'dark-plus',
@@ -211,29 +211,34 @@ export async function buildMDX(
 
   const { remarkAdmonitions, remarkEmoji, highlighter, withShiki, serialize, rehypeSlug } = await MdxDeps;
 
+  const remarkPlugins: NonNullable<SerializeOptions['mdxOptions']>['remarkPlugins'] = [
+    [
+      withShiki,
+      {
+        highlighter,
+      },
+    ],
+    remarkEmoji,
+    ...extraRemarkPlugins,
+  ];
+
+  if (remarkAdmonitions) {
+    remarkPlugins.push([
+      remarkAdmonitions,
+      {
+        customTypes: {
+          shell: {
+            keyword: 'shell',
+            svg: '<svg xmlns="http://www.w3.org/2000/svg" width="16pt" height="16pt" viewBox="0 0 16 16"><path d="M0 0v16h16V0zm15.063 15.063H.937v-11h14.126zm0-11.938H.937V.937h14.126zm0 0"/><path d="M1.875 1.563h.938V2.5h-.938zm0 0M3.438 1.563h.937V2.5h-.938zm0 0M5 1.563h.938V2.5H5zm0 0M1.875 5.074v1.348l.988.637-.988.578V9.05l2.828-1.668v-.586zm0 0M5.34 7.559h1.027v1.226H5.34zm0 0M5.34 5.32h1.027v1.23H5.34zm0 0M6.8 8.785h2.356v1.137H6.801zm0 0"/></svg>',
+          },
+        },
+      },
+    ]);
+  }
+
   const mdx = await serialize(content, {
     mdxOptions: {
-      remarkPlugins: [
-        [
-          remarkAdmonitions,
-          {
-            customTypes: {
-              shell: {
-                keyword: 'shell',
-                svg: '<svg xmlns="http://www.w3.org/2000/svg" width="16pt" height="16pt" viewBox="0 0 16 16"><path d="M0 0v16h16V0zm15.063 15.063H.937v-11h14.126zm0-11.938H.937V.937h14.126zm0 0"/><path d="M1.875 1.563h.938V2.5h-.938zm0 0M3.438 1.563h.937V2.5h-.938zm0 0M5 1.563h.938V2.5H5zm0 0M1.875 5.074v1.348l.988.637-.988.578V9.05l2.828-1.668v-.586zm0 0M5.34 7.559h1.027v1.226H5.34zm0 0M5.34 5.32h1.027v1.23H5.34zm0 0M6.8 8.785h2.356v1.137H6.801zm0 0"/></svg>',
-              },
-            },
-          },
-        ],
-        [
-          withShiki,
-          {
-            highlighter,
-          },
-        ],
-        remarkEmoji,
-        ...extraRemarkPlugins,
-      ],
+      remarkPlugins,
       rehypePlugins: [rehypeSlug, ...extraRehypePlugins],
     },
   });
