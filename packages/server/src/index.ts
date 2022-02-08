e6415a53bf6225bb331e5de65a41aa4f01f6078b
PabloSzx <pablosaez1995@gmail.com>
2022-02-08 12:31:31 -0300

    Fix config, config types & add esmExternals by default

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -7,17 +7,21 @@ export interface WithGuildDocsOptions extends NextConfig {
 
 export * from '@guild-docs/types';
 
-export function withGuildDocs({ env = {}, getRoutes, ...rest }: WithGuildDocsOptions) {
+export function withGuildDocs({ env, getRoutes, ...rest }: WithGuildDocsOptions): NextConfig {
   return {
-    env: {
-      SERIALIZED_MDX_ROUTES: JSON.stringify(getRoutes()),
-      ...env,
-    },
     // Disable eslint by default, it can be overriden
     eslint: {
       ignoreDuringBuilds: true,
     },
     ...rest,
+    env: {
+      SERIALIZED_MDX_ROUTES: JSON.stringify(getRoutes()),
+      ...env,
+    },
+    experimental: {
+      esmExternals: true,
+      ...rest.experimental,
+    },
   };
 }
 
