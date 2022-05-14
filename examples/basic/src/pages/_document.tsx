e3296b1e5f1e9ced28772820cdb65ea940a0efa1
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-05-14 09:02:21 -0400

    Update examples/basic/src/pages/_document.tsx
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

--- examples/basic/src/pages/_document.tsx
+++ examples/basic/src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
-          ></link>
+          />
         </head>
         <body>
           <ColorModeScript initialColorMode="light" />
