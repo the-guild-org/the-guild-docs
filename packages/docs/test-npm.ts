aac34f5f1bf8b8de6b727e78e18e329ab90ee8fd
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-07-25 16:27:33 -0400

    use bob the bundler

--- packages/docs/test-npm.ts
+++
@@ -1,9 +0,0 @@
-import { fetch } from 'undici';
-
-const response1 = await fetch(`https://registry.npmjs.org/${encodeURIComponent('bob-tsm')}`);
-
-console.log(await response1.json());
-
-const response2 = await fetch(`https://api.npmjs.org/downloads/point/last-week/${encodeURIComponent('bob-tsm')}`);
-
-console.log(await response2.json());
